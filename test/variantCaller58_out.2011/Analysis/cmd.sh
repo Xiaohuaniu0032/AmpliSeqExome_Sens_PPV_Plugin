d4e0e5a4ad7e98e07cb44a2e8f2cf807b3e5560b
longfei.fu <longfei.fu@thermofisher.com>
2022-09-21 13:14:12 +0800

    update

---
+++ test/variantCaller58_out.2011/Analysis/cmd.sh
@@ -0,0 +1,1 @@
+perl ../../../AmpliSeqExome_Sens_PPV_Plugin_pipeline.pl /data/fulongfei/git_repo/AmpliSeqExome_Sens_PPV_Plugin/test/variantCaller58_out.2011 /data/fulongfei/database/ref/hg19/hg19.fasta $PWD
